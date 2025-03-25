dd020fb2b201dc602d06311d6ddbb5fa2aa3f947
Matheus <145228073+MatheusLuizTdSilva@users.noreply.github.com>
2025-03-25 19:29:13 -0300

    Update historia.py
    Adicionei um animal para assustá-los

--- atividade_historia/historia.py
+++ atividade_historia/historia.py
@@ -2,5 +2,6 @@ nome = input("Digite o nome de uma pessoa: ")
 nome2 = input("Digite o nome de putra pessoa: ")
 lugar = input("Digite um lugar: ")
 comida = input("Digite um prato de comida: ")
+animal = input("Digite um animal: ")
 
-print(f"Em um dia ensolarado, {nome} e {nome2} decidiram explorar {lugar}. Depois de muitas aventuras, eles pararam em um restaurante e saborearam um delicioso prato de {comida}. Foi um dia inesquecível!")
+print(f"Em um dia ensolarado, {nome} e {nome2} decidiram explorar {lugar}. Depois de muitas aventuras, eles pararam em um restaurante e saborearam um delicioso prato de {comida}. Foi um dia inesquecível!\n Depois andando por {lugar} eles se assustam com um animal! Um(a) {animal}.")
